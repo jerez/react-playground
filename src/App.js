e1536c638a1c947a3054e7ba29cfec9fe53c6dc4
Carlos Jerez <jerez@users.noreply.github.com>
2018-05-09 14:46:30 -0500

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 import Header from './Header';
+
+
+const SceneContainer = (component) => <div><component/></div>;
+
+const Main = () => <h3>Main</h3>
+const First = () => <h3>First</h3>
+const Second = () => <h3>Second</h3>
+
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Header />
-        <main className="app-main">
-          this is the main section
-        </main>
+        <Router>
+          <Fragment>
+            <Header />
+            <main className="app-main">
+              <h2>this is the main section</h2>
+              <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route path="/first" component={First}/>
+                <Route path="/second" component={Second}/>
+              </Switch>
+            </main>
+          </Fragment>
+        </Router>
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+
 
 const Header = () => 
     <header className="app-header">
         <h1 className="app-title">This is a dumb Header</h1>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/first">First</Link>
+          </li>
+          <li>
+            <Link to="/second">Second</Link>
+          </li>
+        </ul>
     </header>
 
 export default Header;
