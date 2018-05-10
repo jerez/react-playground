b92afa153aa10f484a1108b1610b10ce5b10d30e
Carlos Jerez <jerez@users.noreply.github.com>
2018-05-10 10:24:47 -0500

    Split Header render options

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React,  {Fragment} from 'react';
+import { Link, Switch, Route } from 'react-router-dom'
+
+const HeaderContent = () =>
+    <Fragment>
+        <h1 className="app-title">This is Header</h1>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/first">First</Link>
+        </li>
+        <li>
+            <Link to="/second">Second</Link>
+        </li>
+        </ul>
+    </Fragment>
 
 const Header = () => 
     <header className="app-header">
-        <h1 className="app-title">This is a dumb Header</h1>
-        <ul style={{ listStyleType: "none", padding: 0 }}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/first">First</Link>
-          </li>
-          <li>
-            <Link to="/second">Second</Link>
-          </li>
-        </ul>
+        <HeaderContent />
+        <Switch>
+            <Route exact path="/" render={ 
+                () => <span>i'm in home</span>
+            }/>
+            <Route path="/first" render={
+                () => <span>i'm in first</span>
+            }/>
+            <Route path="/second" render={
+                () => <span>i'm in second</span>
+            }/>
+        </Switch>
     </header>
 
-export default Header;  
+const HeaderWithProps = ({someProp}) => 
+    <header className="app-header">
+        <HeaderContent />
+        {someProp ?
+            (<span>I HAVE PROP A</span>) 
+            :
+            (<span>I DO NOT HAVE PROP A</span>)
+        }
+    </header>
+
+const ComposedHeader = () =>
+<Switch>
+  <Route exact path="/" render={()=><HeaderWithProps someProp/>}/>
+  <Route path="/first" render={()=><HeaderWithProps someProp/>}/>
+  <Route component={HeaderWithProps}/>
+</Switch>
+    
+
+export default ComposedHeader;  
