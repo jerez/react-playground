b5ba48ebf0e80bc7e99882563eaabe967f377459
Carlos Jerez <jerez@users.noreply.github.com>
2018-05-10 10:45:22 -0500

    react-router-config example

--- src/RouteContainer.js
+++ src/RouteContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config'
 
-import {Main, First, Second } from './Scenes';
+import {Main, First, Second, Child, GrandChild } from './Scenes';
 
 const RouteContainer = () => 
 <main className="app-main">
@@ -13,6 +14,40 @@ const RouteContainer = () =>
     </Switch>
 </main> 
 
+
+// const RouteContainerConfig = ({ route }) => 
+// <main className="app-main">
+//     <h2>this is the main section with Config</h2>
+//     {/* child routes won't render without this */}
+//     {renderRoutes(routes)}
+// </main> 
+
+// const routes = [
+//     { component: RouteContainerConfig,
+//       routes: [
+//         { path: '/',
+//           exact: true,
+//           component: Main
+//         },
+//         { path: '/first',
+//           exact: true,
+//           component: First
+//         },
+//         { path: '/second',
+//           exact: true,
+//           component: Main
+//         },
+//         { path: '/child/:id',
+//           component: Child,
+//           routes: [
+//             { path: '/child/:id/grand-child',
+//               component: GrandChild
+//             }
+//           ]
+//         }
+//       ]
+//     }
+//   ]
 
 export default RouteContainer;
 
--- src/Scenes.js
+++ src/Scenes.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { renderRoutes } from 'react-router-config'
 
 export const SceneContainer = (component) => <div><component/></div>;
 
 export const Main = () => <h3>Main</h3>
 export const First = () => <h3>First</h3>
 export const Second = () => <h3>Second</h3>
+
+export const Child = ({ route }) => (
+    <div>
+      <h2>Child</h2>
+      {/* child routes won't render without this */}
+      {renderRoutes(route.routes, { someProp: 'these extra props are optional' })}
+    </div>
+  )
+  
+export const GrandChild = ({ someProp }) => (
+    <div>
+        <h3>Grand Child</h3>
+        <div>{someProp}</div>
+    </div>
+)
